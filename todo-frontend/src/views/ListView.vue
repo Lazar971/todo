ed9d157810412f13c72bf16624c3215c5a175faa
Lazar971 <lazar.milosavljevic97@gmail.com>
2020-01-29 12:55:07 +0100

    created components navbar,list and listview. Created view listview

---
+++ todo-frontend/src/views/ListView.vue
@@ -0,0 +1,20 @@
+<template>
+ 
+  <div>
+    
+    <span>TEST 123</span>
+    <Navbar />
+    <List />
+  </div>
+</template>
+<script>
+import Navbar from "@/components/NavBar.vue";
+import List from "@/components/List.vue";
+export default {
+  
+  components: {
+    Navbar,
+    List
+  }
+};
+</script>
